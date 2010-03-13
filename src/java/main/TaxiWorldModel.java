87ebad5bb6bb236e30918d36c7bc19be701e8303
adamtaylor <adamjctaylor@gmail.com>
2010-03-13 20:30:43 +0000

    adding the rest of the eclipse files

--- src/java/main/TaxiWorldModel.java
+++ src/java/main/TaxiWorldModel.java
@@ -3,10 +3,28 @@ package main;
 import jason.environment.grid.GridWorldModel;
 
 public class TaxiWorldModel extends GridWorldModel {
-
+    private boolean hasCustomer = false;
+    private boolean isAtTaxiRank = false;
+    
     protected TaxiWorldModel(int arg0, int arg1, int arg2) {
         super(arg0, arg1, arg2);
         // TODO Auto-generated constructor stub
+    }
+    
+    public boolean getCustomer() {
+        if (isAtTaxiRank) {
+            hasCustomer = true;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean dropCustomer() {
+        if (hasCustomer) {
+            hasCustomer = false;
+            return true;
+        }
+        return false;
     }
 
 }
---
+++ src/java/main/TestEnv.java
@@ -0,0 +1,31 @@
+package main;
+
+// Environment code for project MAS
+
+import jason.asSyntax.*;
+import jason.environment.*;
+import java.util.logging.*;
+
+public class TestEnv extends Environment {
+
+    private Logger logger = Logger.getLogger("MAS."+TestEnv.class.getName());
+
+    /** Called before the MAS execution with the args informed in .mas2j */
+    @Override
+    public void init(String[] args) {
+        super.init(args);
+        addPercept(Literal.parseLiteral("percept(demo)"));
+    }
+
+    @Override
+    public boolean executeAction(String agName, Structure action) {
+        logger.info("executing: "+action+", but not implemented!");
+        return true;
+    }
+
+    /** Called before the end of MAS execution */
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
